33afc9e3bd3c27c155ed1347f0a9608eb0af8501
Anurag Khandelwal <anuragk@berkeley.edu>
2018-01-12 11:48:31 -0800

    Added throughput measurement for storm

--- storm-bench/src/main/java/pkt/benchmark/PacketTopology.java
+++ storm-bench/src/main/java/pkt/benchmark/PacketTopology.java
@@ -12,12 +12,16 @@ import org.apache.storm.topology.base.BaseRichSpout;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 import java.util.Map;
 
 public class PacketTopology {
-    private static final byte[] pktData = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    static Logger LOG = LoggerFactory.getLogger(PacketTopology.class);
+
+    private static final byte[] pktData = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
     public static class PktSpout extends BaseRichSpout {
         SpoutOutputCollector _collector;
@@ -58,11 +62,64 @@ public class PacketTopology {
         }
     }
 
+    public static class ThroughputBolt extends BaseRichBolt {
+        long received = 0;
+        long start = 0;
+        private OutputCollector _collector;
+        private long logfreq;
+        private long lastLog = -1;
+        private long lastPkts;
+
+        ThroughputBolt(long logfreq) {
+            this.logfreq = logfreq;
+        }
+
+        @Override
+        public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+            _collector = collector;
+        }
+
+        @Override
+        public void execute(Tuple input) {
+            if (start == 0) {
+                start = System.currentTimeMillis();
+            }
+            received++;
+            if (received % logfreq == 0) {
+                long now = System.currentTimeMillis();
+
+                // throughput for the last "logfreq" elements
+                if (lastLog == -1) {
+                    // init (the first)
+                    lastLog = now;
+                    lastPkts = received;
+                } else {
+                    long timeDiff = now - lastLog;
+                    long pktDiff = received - lastPkts;
+                    double ex = (1000 / (double) timeDiff);
+                    LOG.info("During the last {} ms, we received {} pkts. That's {} pkts/second/core", timeDiff,
+                            pktDiff, pktDiff * ex);
+                    // reinit
+                    lastLog = now;
+                    lastPkts = received;
+                }
+            }
+
+            _collector.ack(input);
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("pkt"));
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         TopologyBuilder builder = new TopologyBuilder();
 
         builder.setSpout("pkt", new PktSpout(), 1);
-        builder.setBolt("processed_pkts", new NoOpBolt(), 1).shuffleGrouping("pkt");
+        // builder.setBolt("processed_pkt", new NoOpBolt(), 1).shuffleGrouping("pkt");
+        builder.setBolt("thput_pkt", new ThroughputBolt(1_000_000), 1).shuffleGrouping("pkt");
 
         LocalCluster cluster = new LocalCluster();
         cluster.submitTopology("test", new Config(), builder.createTopology());
